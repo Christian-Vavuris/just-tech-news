91273a0a421bd94d06693068dd35ea70be3dbac5
Christian Vavuris <christianvavuris@Christians-MacBook-Pro.local>
2020-08-17 21:03:46 -0700

    need to add in the Comment modal from last week's lesson

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 
 
 app.engine('handlebars', hbs.engine);
-app.set('view enginer', 'handlebars');
+app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
