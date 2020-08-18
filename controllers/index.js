9d417524affafde442ccdc8d37425980f8a23f63
Christian Vavuris <christianvavuris@Christians-MacBook-Pro.local>
2020-08-17 20:35:28 -0700

    bug fixin

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,10 @@
-
-
+const homeRoutes = require('./home-routes.js');
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
