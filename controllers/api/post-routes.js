4da63b3354cbd59afc3c3c6a0ce795fb3f631d02
Christian Vavuris <christianvavuris@Christians-MacBook-Pro.local>
2020-08-22 10:51:08 -0700

    added in DELETE, PUT, and GET routes for Posts

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -2,13 +2,16 @@
 
 const router = require('express').Router();
 const { Post, User } = require('../../models');
+// const { json } = require('sequelize/types');
+const { response } = require('express');
 // const { UniqueConstraintError } = require('sequelize/types');   Where did this come from??
 
 // get all users
 router.get('/', (req, res) => {
     Post.findAll({
       attributes: ['id', 'post_url', 'title', 'created_at'],
-      include: [
+      order: [['created_at', "DESC"]], 
+      include: [  // this is a join statement
         {
           model: User,
           attributes: ['username']
@@ -23,8 +26,97 @@ router.get('/', (req, res) => {
   });
 
 
+// get a specific user
+
+router.get('/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: ['id', 'post_url', 'title', 'created_at'],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  router.post('/', (req, res) => {
+      // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+  Post.create({
+    title: req.body.title, 
+    post_url: req.body.post_url,
+    user_id: req.body.user_id
+  })
+  .then(dbPostData => res.json(dbPostData))
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
+});
+
+// Update a Post's Title
+router.put('/:id', (req, res) => {
+    Post.update(
+        {
+            title: req.body.title
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+        .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+})
+
+//Delete a post
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+})
+
+
+
 
 module.exports = router;
 
 
-// I'm running into another SQL database issue and I'm not sure how to resolve it
+
